8f8c165b4b6a01986c68c0fe8247fb5bac73c0cf
mahmudul-Hasan-2 <mahmudul5709@gmail.com>
2026-03-21 23:34:23 +0600

    Has complete week-1 task

---
+++ Week-1/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Greeting from './Greeting'
+
+function App() {
+
+  return (
+    <>
+      <Greeting name="Mahmudul Hasan Nirab"></Greeting>
+    </>
+  )
+}
+
+export default App
---
+++ Week-1/src/Greeting.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Greeting = ({ name }) => {
+    return (
+        <div>
+            <h2>Hello, {name}! Welcome to ReactSphere.</h2>
+        </div>
+    );
+};
+
+export default Greeting;
